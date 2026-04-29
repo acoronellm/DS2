44836ed3a0cf38cf070620839fe4cf8aa80ebdc8
acoronellm <151686541+acoronellm@users.noreply.github.com>
2026-04-28 20:16:49 -0500

    mejoras de n8n

--- n8n/init.sh
+++ n8n/init.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-echo "Esperando a que n8n esté listo..."
-sleep 10
+echo "Esperando n8n..."
+
+while ! wget -qO- http://localhost:5678 >/dev/null 2>&1; do
+  sleep 2
+done
+
+echo "n8n listo"
 
 echo "Importando workflow..."
 n8n import:workflow --input=/workflows/text-flow.json
 
+echo "Importando credenciales..."
+n8n import:credentials --input=/credentials.json
+
 echo "Iniciando n8n..."
-exec n8n start
+exec n8n startpor 
